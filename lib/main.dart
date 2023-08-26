5df942eed2faf5e9afd0568a12c33c3a1c54db40
Wellington Lima <wellington.lima@capesesp.com.br>
2023-08-26 18:30:12 -0300

    Aula 8

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:asp/asp.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:thing_stuffs/src/app_module.dart';
+import 'package:thing_stuffs/src/app_widget.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(RxRoot(
+    child: ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  ));
+}
---
+++ lib/src/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      routerConfig: Modular.routerConfig,
+    );
+  }
+}
+
